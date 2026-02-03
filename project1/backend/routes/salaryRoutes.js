68e81f8c6a5341a4fd31c280fb6f665e5872e21c
Tushar Bheda <phantomdx111@gmail.com>
2026-02-03 17:30:26 +0530

    Delete project1 directory

--- project1/backend/routes/salaryRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createSalary } from "../controllers/salarycontroller.js";
-
-const router = express.Router();
-
-
-router.post("/", createSalary);
-
-export default router;
